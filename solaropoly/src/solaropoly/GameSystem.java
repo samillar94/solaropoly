e8c78c372f62ab86518cfb4ce30bc6818a832ad7
Sam Millar <samillar94@outlook.com>
2023-03-18 14:55:39 +0000

    Gameplay loop implemented

--- solaropoly/src/solaropoly/GameSystem.java
+++ solaropoly/src/solaropoly/GameSystem.java
@@ -12,50 +12,86 @@ import java.util.Scanner;
  */
 public class GameSystem {
 
-	// game parameter constants
-	
+	/// game parameter constants
+
 	public static final int STARTING_BALANCE = 15000000;
 	public static final int MIN_PLAYERS = 2;
 	public static final int MAX_PLAYERS = 4;
-	public static final String CURRENCY_SYMBOL = "kWh";
-	
-	// essential components
-	
+	public static final String PRE = ""; /// resource prefix
+	public static final String SUF = "kWh"; /// resource suffix
+
+	/// essential components
+
 	public static final Scanner SCANNER = new Scanner(System.in);
 
 	private static boolean gameEndTrigger = false;
 
 	public static ArrayList<Player> players = new ArrayList<Player>();
-	
+
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 
-		
-		// set number of players
-		setNumPlayers();
-		
-		// register players
-		players = registerPlayers();
-		//int playersTotal = players.size();
-		
-		// game starts - do while, cycling through players
-		
+		try {
+
+			/// set number of players
+			setNumPlayers();
+
+			/// register players
+			players = registerPlayers();
+
+			/// game starts - do while (and try-catch?), cycling through players until game
+			/// end triggered
+			while (!gameEndTrigger) {
+
+				for (int playerIndex = 0; playerIndex < players.size(); playerIndex++) {
+
+					Thread.sleep(2000);
+
+					turn(players.get(playerIndex));
+
+					if (gameEndTrigger)	break;
+
+				}
+
+			}
+			
+			/// final code to show balances and assets
+			
+
+		} catch (InterruptedException e) {
+
+			System.out.println("Sorry, the sleep was interrupted and the softwear engine crashed.");
+			e.printStackTrace();
+
+		} catch (Exception e) {
+
+			System.out.println("Sorry, something went wrong and the softwear engine crashed.");
+			e.printStackTrace();
+
+		}
+
 	}
-	
+
 	private static int setNumPlayers() {
-		
+
 		int num = 0;
-		// TODO scanner code
+		/// TODO scanner code
 		players = new ArrayList<Player>(num);
 		return num;
-		
+
 	}
-	
+
 	private static ArrayList<Player> registerPlayers() {
-		// TODO scanner fo
+		/// TODO scanner
 		ArrayList<Player> playerBuilder = new ArrayList<Player>();
 		return playerBuilder;
 	}
+
+	private static void turn(Player player) {
+		/// TODO turn mechanism
+
+	}
+
 }
