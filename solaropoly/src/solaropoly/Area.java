ab871e0b0118ae0b492bc0e90606705a59b297b0
qu1zzyboy <litterpigger@gmail.com>
2023-03-24 14:57:31 +0000

    change test

--- solaropoly/src/solaropoly/Area.java
+++ solaropoly/src/solaropoly/Area.java
@@ -12,18 +12,39 @@ import java.util.stream.Collectors;
  * This is the square that actually does stuff! Areas can be owned and developed by players.
  * @author G17
  */
-public class Area extends Square implements GeneratesIncome {
+public class Area extends Square {
 	
 	private Group group;
 	private Player owner;
 	private int cost;
-	private HashMap<String, ArrayList<Integer>> rentProfile = new HashMap<>(2);
+	private ArrayList<Integer> rentProfile=new ArrayList<>();
 	private int monopolyLevel = 0;
 	private int developmentLevel = 0;
-
+private String name;
 	
 	/// setget
 	
+	/**
+ * @return the name
+ */
+public String getName() {
+	return name;
+}
+
+/**
+ * @param name the name to set
+ */
+public void setName(String name) {
+	this.name = name;
+}
+
+/**
+ * @param rentProfile the rentProfile to set
+ */
+public void setRentProfile(ArrayList<Integer> rentProfile) {
+	this.rentProfile = rentProfile;
+}
+
 	/**
 	 * @return the field
 	 */
@@ -55,35 +76,6 @@ public class Area extends Square implements GeneratesIncome {
 	/**
 	 * @return the cost
 	 */
-	@Override
-	public int getCost() {
-		return cost;
-	}
-
-	/**
-	 * @param cost the cost to set
-	 */
-	@Override
-	public void setCost(int cost) {
-		this.cost = cost;
-	}
-	
-	/**
-	 * @return the rentProfile
-	 */
-	public HashMap<String, ArrayList<Integer>> getRentProfile() {
-		return rentProfile;
-	}
-
-	/**
-	 * @param rentProfile the rentProfile to set
-	 */
-	public void setRentProfile(HashMap<String, ArrayList<Integer>> rentProfile) {
-		// TODO validate HashMap and ArrayList not empty, Integer 0+
-		this.rentProfile = rentProfile;
-	}
-
-	
 	
 
 	/**
@@ -182,8 +174,13 @@ public class Area extends Square implements GeneratesIncome {
 	 * @author Li
 	 * 
 	 */
-public void rentPay(Player p1, Square s1) {
+public void rentPay(Player p2, Area a2) {
 	// TODO When players landed on others land
+	
+	p2.setBalance(p2.getBalance()-rentProfile.get(a2.developmentLevel+a2.monopolyLevel));
+	a2.getOwner().setBalance(p2.getBalance()-rentProfile.get(a2.developmentLevel+a2.monopolyLevel));
+	//change test
+	
 }
 	
 
